b399b99edd1e12da85492a8058078d14e388e0b2
Wilcson <wilcsonaraujo@outlook.com>
2026-06-25 21:10:17 -0300

    Adicionado teste unitario

--- backend/src/core/ai/gemini_provider.py
+++ backend/src/core/ai/gemini_provider.py
@@ -1,26 +1,28 @@
 import json
-import types
+from google.genai import types
 from typing import Any, Dict
 
 from google import genai
-from backend.src.core.ai import AIProvider
-from backend.src.core import settings
+from backend.src.core.ai.provider import AIProvider
+from backend.src.core.config import settings
 
 
-class gemini_provider(AIProvider):
+class GeminiProvider(AIProvider):
     def __init__(self, api_key: str = settings.GEMINI_API_KEY):
         if not api_key:
             raise ValueError("The Gemini API key was not found.")
-        
+
         self.client = genai.Client(api_key)
         self.gemini_model = "gemini-1.5-flash"
 
     def _call_model(self, prompt: str, config=None) -> str:
-        response = self.client.models.generate_content(model=self.gemini_model, content=prompt, config=config)
+        response = self.client.models.generate_content(
+            model=self.gemini_model, contents=prompt, config=config
+        )
         return response.text
 
     def generate_json(self, prompt: str, schema: Any = None) -> Dict[str, Any]:
-        
+
         config_args = {"response_mime_type": "application/json"}
 
         if schema:
@@ -28,8 +30,7 @@ class gemini_provider(AIProvider):
 
         config = types.GenerateContentConfig(**config_args)
 
-        response = self._call_model()
+        response = self._call_model(prompt=prompt, config=config)
         response = response.strip()
 
         return json.loads(response)
-        
